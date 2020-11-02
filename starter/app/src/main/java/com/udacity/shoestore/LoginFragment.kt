7a6d8ae598c0bd9d666423b708b31403aa3fc310
Abhijeet Upadhyay <abhijeet.upahyay@gmail.com>
2020-11-02 19:19:14 +0530

    Why:
    * Step 1 - 6
    
    This change addresses the need by:
    
    *

---
+++ starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        
+        var binding:FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.existingButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragment2ToWelcomeFragment())
+        }
+
+        binding.newLoginButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragment2ToWelcomeFragment())
+        }
+
+        return binding.root
+    }
+}
